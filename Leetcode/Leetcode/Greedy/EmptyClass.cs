125e100762987fa65abe2f4ac4fa758d7824af04
Jingjie_Jiang <jingjiejiang85@gmail.com>
2018-12-17 00:29:13 +0800

    330. Patching Array

--- Leetcode/Leetcode/Greedy/EmptyClass.cs
+++ Leetcode/Leetcode/Greedy/EmptyClass.cs
@@ -3,5 +3,30 @@ namespace Leetcode.Greedy
 {
     public class EmptyClass
     {
+        public static int MinPatches(int[] nums, int n)
+        {
+            int count = 0;
+            long sum = 0;
+
+            for (int idx = 0; idx < nums.Length; idx++)
+            {
+                int limit = Math.Min(nums[idx] - 1, n);
+                while (sum < limit)
+                {
+                    sum += sum + 1;
+                    count++;
+                }
+                sum += nums[idx];
+            }
+
+            while (sum < n)
+            {
+                sum += sum + 1;
+                count++;
+            }
+
+            return count;
+        }
+
     }
 }
