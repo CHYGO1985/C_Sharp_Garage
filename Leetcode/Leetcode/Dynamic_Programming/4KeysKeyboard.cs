fe26611cdd1f049658b3ceb4dfeeab78c9f4e5a0
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-01-07 23:45:59 +0800

    651. 4 Keys Keyboard

---
+++ Leetcode/Leetcode/Dynamic_Programming/4KeysKeyboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Leetcode
+{
+    public class _KeysKeyboard
+    {
+        public int MaxA(int N)
+        {
+            int[] res = new int[N + 1];
+            res[0] = 0;
+
+            for (int idx = 0; idx < N; idx++)
+            {
+                if (idx <= 6)
+                {
+                    res[idx] = idx;
+                }
+                else
+                {
+                    res[idx] = Math.Max(res[idx - 3] * 2, Math.Max(res[idx - 4] * 3,
+                        Math.Max(res[idx - 5] * 4, idx)));
+                }
+            }
+
+            return res[N];
+        }
+    }
+}
