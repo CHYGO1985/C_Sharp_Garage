4c1911c9ae13637aa6cc82573800d974e318e746
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-19 23:05:35 +0800

    346. Moving Average from Data Stream

---
+++ Leetcode/Leetcode/Queue/MovingAverage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode.Queue
+{
+    public class MovingAverage
+    {
+        private Queue<int> queue;
+        private int size;
+        private int sum;
+
+        /** Initialize your data structure here. */
+        public MovingAverage(int size)
+        {
+            queue = new Queue<int>();
+            this.size = size;
+            sum = 0;
+        }
+
+        public double Next(int val)
+        {
+            if (queue.Count < size)
+            {
+                queue.Enqueue(val);
+                sum += val;
+            }
+            else
+            {
+                int old = queue.Dequeue();
+                queue.Enqueue(val);
+                sum = sum - old + val;
+            }
+
+            return (double)sum / queue.Count;
+        }
+    }
+}
