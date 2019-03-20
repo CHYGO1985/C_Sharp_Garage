40e7892ef81770d7ffaad6a5138512493d4e586e
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-20 23:47:33 +0800

    344. Reverse String

---
+++ Leetcode/Leetcode/String/ReverseString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Leetcode.String
+{
+    public class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            int head = 0, rear = s.Length - 1;
+            while(head < rear)
+            {
+                char tmp = s[head];
+                s[head ++] = s[rear];
+                s[rear --] = tmp;
+            }
+        }
+    }
+}
