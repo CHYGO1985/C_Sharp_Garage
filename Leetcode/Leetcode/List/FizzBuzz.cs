e6d61bdbb18ceedf8ec06cded650897632805797
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-25 21:22:07 +0800

    412. Fizz Buzz

---
+++ Leetcode/Leetcode/List/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode.List
+{
+    public class FizzBuzz
+    {
+        public IList<string> FizzBuzz1(int n)
+        {
+            List<string> list = new List<string>();
+
+            List<string> list = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    list.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    list.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    list.Add("Buzz");
+                }
+                else
+                {
+                    list.Add(i.ToString());
+                }
+            }
+
+            return list;
+        }
+    }
+}
+}
