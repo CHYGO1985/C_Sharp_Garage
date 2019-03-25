c3a9a2a0f8c8fad1bf24ab2c2f2d7bb46a2fd60a
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-26 01:01:01 +0800

    917. Reverse Only Letters

---
+++ Leetcode/Leetcode/String/ReverseOnlyLetters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Leetcode.String
+{
+    public class ReverseOnlyLetters
+    {
+        public string ReverseOnlyLetters1(string S)
+        {
+            var chars = S.ToCharArray();
+
+            for (int head = 0, rear = S.Length - 1; head < rear; head ++, rear --)
+            {
+                while (head < rear && Char.IsLetter(chars[head]) == false) head ++;
+                while (head < rear && char.IsLetter(chars[rear]) == false) rear --;
+
+                if (head < rear)
+                {
+                    char tmp = chars[head];
+                    chars[head] = chars[rear];
+                    chars[rear] = tmp;
+                }
+            }
+
+            return new string(chars);
+        }
+    }
+}
