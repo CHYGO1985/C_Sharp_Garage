6e861914329e75e67d7e086695fe52633be423be
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-28 00:07:50 +0800

    784. Letter Case Permutation

---
+++ Leetcode/Leetcode/Greedy/LetterCasePermutation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * 
+ * @jingjiejiang Mar 27, 2019
+ * 
+ */
+namespace Leetcode.Greedy
+{
+    public class LetterCasePermutation
+    {
+        public IList<string> LetterCasePermutation1(string S)
+        {
+            char[] arr = S.ToCharArray();
+            List<string> list = new List<string>();
+            dfs(list, 0, arr);
+
+            return list;
+        }
+
+        private void dfs(List<string> list, int index, char[] arr)
+        {
+            if (index >= arr.Length)
+            {
+                list.Add(new string(arr));
+                return ;
+            }
+
+            if (Char.IsDigit(arr[index]))
+            {
+                dfs(list, index + 1, arr);
+            }
+            else
+            {
+                dfs(list, index + 1, arr);
+                arr[index] = Char.IsLower(arr[index]) ? Char.ToUpper(arr[index]) : Char.ToLower(arr[index]);
+                dfs(list, index + 1, arr);
+            }
+        }
+    }
+}
