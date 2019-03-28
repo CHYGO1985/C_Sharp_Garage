f1213dc67af8569f0a32ceb63753173b671c2e99
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-28 22:24:38 +0800

    349. Intersection of Two Arrays

---
+++ Leetcode/Leetcode/Hash/IntersectOfTwoArrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+/**
+ * 
+ * @jingjiejiang Mar 28, 2019
+ */
+namespace Leetcode.Hash
+{
+    public class Solution
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> hashSet = new HashSet<int>(nums1);
+            HashSet<int> resSet = new HashSet<int>();
+            foreach (int num in nums2)
+            {
+                if (hashSet.Contains(num))
+                {
+                    resSet.Add(num);
+                }
+            }
+
+            int[] resArr = new int[resSet.Count];
+            resSet.CopyTo(resArr);
+
+            return resArr;
+        }
+    }
+}
