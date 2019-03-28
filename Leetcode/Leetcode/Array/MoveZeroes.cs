c80f996c8e02fa3ac2d9200f4fc35dbdc5f2612e
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-28 21:22:35 +0800

    283. Move Zeroes

---
+++ Leetcode/Leetcode/Array/MoveZeroes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Leetcode.Array
+{
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int zeroIdx = 0;
+            while (nums[zeroIdx] != 0 && zeroIdx < nums.Length) zeroIdx++;
+
+            int shiftIdx = zeroIdx + 1;
+            while (shiftIdx < nums.Length)
+            {
+                if (nums[shiftIdx] != 0)
+                {
+                    nums[zeroIdx] = nums[shiftIdx];
+                    nums[shiftIdx] = 0;
+                    zeroIdx++;
+                }
+             
+                shiftIdx ++;
+            }
+        }
+    }
+}
