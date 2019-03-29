095d8967b0c686a01b7a934847bc1247c085baca
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-29 21:35:20 +0800

    206. Reverse Linked List

---
+++ Leetcode/Leetcode/List/ReverseLinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang Mar 29, 2019
+ */
+namespace Leetcode.List
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode headNext = head.next;
+            ListNode newHead = ReverseList(head.next);
+            headNext.next = head;
+            head.next = null;
+            return newHead;
+        }
+    }
+}
