c965238e7d6558267093d88705711487a1e87cc0
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-31 23:03:56 +0800

    13. Roman to Integer

---
+++ Leetcode/Leetcode/Array/RomanToInteger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang Mar 31, 2019
+ */
+namespace Leetcode.Array
+{
+    public class RomanToInteger
+    {
+        public int RomanToInt(string s)
+        {
+            int result = 0;
+            char[] Roman = s.ToCharArray();
+            for (int i = 0; i < Roman.Length; i++)
+            {
+                switch (Roman[i])
+                {
+                    case 'M':
+                        result += 1000;
+                        break;
+                    case 'D':
+                        result += 500;
+                        break;
+                    case 'C':
+                        result += 100;
+                        break;
+                    case 'L':
+                        result += 50;
+                        break;
+                    case 'X':
+                        result += 10;
+                        break;
+                    case 'V':
+                        result += 5;
+                        break;
+                    case 'I':
+                        result += 1;
+                        break;
+                }
+            }
+            if (s.Contains("CD") || s.Contains("CM"))
+                result -= 200;
+            if (s.Contains("XL") || s.Contains("XC"))
+                result -= 20;
+            if (s.Contains("IV") || s.Contains("IX"))
+                result -= 2;
+
+
+            return result;
+
+        }
+    }
+}
