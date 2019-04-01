c514041c952ec9f5522ea96a05186934a0cf80f8
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-04-01 20:52:26 +0800

    242. Valid Anagram

---
+++ Leetcode/Leetcode/String/ValidAnagram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang Apr 1, 2019
+ */
+namespace Leetcode.String
+{
+    public class ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            // assume that s and t will not be null
+            if (s.Length != t.Length) return false;
+            int[] dicts = new int[26];
+
+            for (int idx = 0; idx < s.Length; idx ++)
+            {
+                dicts[s[idx] - 'a'] += 1;
+                dicts[t[idx] - 'a'] -= 1;
+            }
+
+            foreach (int dict in dicts)
+            {
+                if (dict != 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
