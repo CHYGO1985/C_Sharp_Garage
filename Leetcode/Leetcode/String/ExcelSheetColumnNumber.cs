bae97154556e8c19401a4f13dc68853de0c58103
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-04-03 22:02:35 +0800

    171. Excel Sheet Column Number not fast enough, should not use Pow, should leverage previous result

---
+++ Leetcode/Leetcode/String/ExcelSheetColumnNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang April 3, 2019
+ */
+namespace Leetcode.String
+{
+    public class ExcelSheetColumnNumber
+    {
+        public int TitleToNumber(string s)
+        {
+            int sum = 0;
+
+            for (int offset = 0; offset < s.Length; offset ++)
+            {
+                int factor = Convert.ToInt32(Math.Pow(26, offset));
+                sum += (s[s.Length - 1 - offset] - 'A' + 1) * factor;
+            }
+
+            return sum;
+        }
+    }
+}
