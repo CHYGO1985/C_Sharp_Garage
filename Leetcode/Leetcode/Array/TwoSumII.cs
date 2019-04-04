52cf9e84d15a4bc3383a9a4fe99d2681db4b37c8
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-04-04 22:07:30 +0800

    167. Two Sum II - Input array is sorted

---
+++ Leetcode/Leetcode/Array/TwoSumII.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang April 4, 2019
+ */
+namespace Leetcode.Array
+{
+    public class TwoSumII
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            // assume the max sum of two nums in numbers won't > Int.Max
+            int head = 0;
+            int rear = numbers.Length - 1;
+            int[] res = new int[2];
+
+            while (head < rear)
+            {
+                int sum = numbers[head] + numbers[rear];
+                if (sum == target)
+                {
+                    res[0] = head + 1;
+                    res[1] = rear + 1;
+                    break;
+                }
+                else if (sum < target)
+                {
+                    head++;
+                }
+                else
+                {
+                    rear--;
+                }
+            }
+
+            return res;
+        }
+    }
+}
