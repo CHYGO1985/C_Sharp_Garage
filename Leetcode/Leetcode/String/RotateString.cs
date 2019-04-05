af4cf670a7ad546fb12e69525fb06823c6866db8
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-04-05 23:48:21 +0800

    796. Rotate String

---
+++ Leetcode/Leetcode/String/RotateString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang Apr 5, 2019
+ */
+namespace Leetcode.String
+{
+    public class RotateString
+    {
+        public bool RotateString(string A, string B)
+        {
+
+        }
+    }
+}
