1d036c0fd57b672b18430746e7ee6bc616c48563
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-04-06 00:44:25 +0800

    796. Rotate String

--- Leetcode/Leetcode/String/RotateString.cs
+++ Leetcode/Leetcode/String/RotateString.cs
@@ -10,7 +10,21 @@ namespace Leetcode.String
     {
         public bool RotateString(string A, string B)
         {
+            if (A.Length != B.Length) return false;
+            if (A.Equals(B)) return true;
 
+            for (int idx = 0; idx < A.Length; idx ++)
+            {
+                string tmp = A.Substring(idx);
+
+                if (B.IndexOf(tmp, StringComparison.Ordinal) == 0 &&
+                    A.Substring(0, idx).Equals(B.Substring(tmp.Length, idx)))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
