963ca59f51c08c2ea2ba265b5a086b07b7f1bbb9
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-04-07 20:57:19 +0800

    121. Best Time to Buy and Sell Stock

---
+++ Leetcode/Leetcode/Dynamic_Programming/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang April 7, 2019
+ */
+namespace Leetcode.Dynamic_Programming
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int[] profits = new int[2];
+            profits[0] = 0;
+            profits[1] = Int32.MinValue;
+
+            foreach (int price in prices)
+            {
+                profits[0] = Math.Max(profits[0], profits[1] + price);
+                profits[1] = Math.Max(profits[1], -price);
+            }
+
+            return profits[0];
+        }
+    }
+}
