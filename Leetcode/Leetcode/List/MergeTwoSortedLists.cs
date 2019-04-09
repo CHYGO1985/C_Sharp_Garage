31c3fcd58b0abed144cac71380b4ff8bfe32c3a7
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-04-09 23:58:46 +0800

    21. Merge Two Sorted Lists

---
+++ Leetcode/Leetcode/List/MergeTwoSortedLists.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang April 9, 2019
+ * 
+ * 
+ */
+namespace Leetcode.List
+{
+    public class MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists1(ListNode l1, ListNode l2)
+        {
+            if (l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    l1 = l1.next;
+                }
+                else
+                {
+                    ListNode l1Next = l1.next;
+                    ListNode l2Next = l2.next;
+                    l1 = l2;
+                    l1.next = l1Next;
+                    l2 = l2Next;
+                }
+
+                return MergeTwoLists1(l1, l2);
+            }
+
+            return l1;
+        }
+    }
+}
