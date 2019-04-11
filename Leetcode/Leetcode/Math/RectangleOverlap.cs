b25a4fe20530be2ad4a2ce985da84aab281c88fa
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-11 15:23:09 +0800

    836. Rectangle Overlap

---
+++ Leetcode/Leetcode/Math/RectangleOverlap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang April 11, 2019
+ */
+public class Class1
+{
+    public bool IsRectangleOverlap(int[] rec1, int[] rec2)
+    {
+
+        int x00 = rec1[0];
+        int y00 = rec1[1];
+        int x01 = rec1[2];
+        int y01 = rec1[3];
+        int x10 = rec2[0];
+        int y10 = rec2[1];
+        int x11 = rec2[2];
+        int y11 = rec2[3];
+        return x00 < x11 && x10 < x01 && y00 < y11 && y10 < y01;
+    }
+}
---
+++ Leetcode/Leetcode/String/DiameterOfBinaryTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/**
+ * 
+ * @jingjiejiang April 10, 2019
+ * 
+ */
+public class Class1
+{
+    private int sum = 0;
+
+    public int DiameterOfBinaryTree(TreeNode root)
+    {
+
+        if (root == null) return 0;
+
+        helper(root);
+
+        return sum - 1;
+    }
+
+    private int helper(TreeNode root)
+    {
+
+        if (root == null) return 0;
+
+        int left = helper(root.left);
+        int right = helper(root.right);
+
+        sum = Math.Max(sum, left + right + 1);
+
+        return Math.Max(left, right) + 1;
+    }
+}
