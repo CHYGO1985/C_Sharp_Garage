4ed4457dd771aeaa47f6eb1f040bb089f533b54c
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-12 16:02:12 +0800

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ Leetcode/Leetcode/Tree/LowestComAncestorOfBiSearchTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang April 12, 2019 
+ */
+public class Class1
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root == null) return null;
+
+        TreeNode left = LowestCommonAncestor(root.left, p, q);
+        TreeNode right = LowestCommonAncestor(root.right, p, q);
+
+        if ((root.val == p.val || root.val == q.val) || (left != null && right != null))
+        {
+            return root;
+        }
+        else
+        {
+            return left == null ? right : left;
+        }
+    }
+}
