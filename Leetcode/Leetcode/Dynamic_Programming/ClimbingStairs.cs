c1f6a3343de306b01022a85f37a255c66e4d37a7
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-12 17:25:04 +0800

    70. Climbing Stairs

---
+++ Leetcode/Leetcode/Dynamic_Programming/ClimbingStairs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Apr 12, 2019
+ */
+public class Class1
+{
+    public int ClimbStairs(int n)
+    {
+        int[] steps = new int[n + 2];
+        steps[0] = 0;
+        steps[1] = 1;
+
+        for (int idx = 1; idx <= n; idx++)
+        {
+            steps[idx + 1] = steps[idx] + steps[idx - 1];
+        }
+
+        return steps[n + 1];
+    }
+}
