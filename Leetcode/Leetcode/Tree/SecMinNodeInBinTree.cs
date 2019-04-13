0679708dd39043f71bef7e838c7a942876c63eba
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-04-13 23:25:03 +0800

    671. Second Minimum Node In a Binary Tree

---
+++ Leetcode/Leetcode/Tree/SecMinNodeInBinTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Apr 13, 2019 
+ */
+namespace Leetcode.Tree
+{
+    public class SecMinNodeInBinTree
+    {
+        bool isChanged = false;
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+
+            int[] range = new int[] { root.val, Int32.MaxValue };
+            findValue(root, range);
+            return isChanged ? range[1] : -1;
+        }
+
+        private void findValue(TreeNode root, int[] range)
+        {
+
+            if (root == null) return;
+
+            if (root.val > range[0] && root.val <= range[1])
+            {
+                range[1] = root.val;
+                isChanged = true;
+            }
+
+
+            findValue(root.left, range);
+            findValue(root.right, range);
+        }
+    }
+}
