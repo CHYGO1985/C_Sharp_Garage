41e739a1fd866468a7700aa4b7e13db8c0729e6c
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-15 13:59:09 +0800

    53. Maximum Subarray

---
+++ Leetcode/Leetcode/Array/MaximumSubarray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Apr 15, 2019
+ */
+public class Class1
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int curSum = nums[0], sum = nums[0];
+
+        for (int idx = 1; idx < nums.Length; idx ++)
+        {
+            if (nums[idx] < 0)
+            {
+                if (curSum >= 0)
+                {
+                    sum = Math.Max(sum, curSum);
+                    curSum += nums[idx];
+                }
+                else // curSum < 0;
+                {
+                    sum = Math.Max(sum, Math.Max(curSum, nums[idx]));
+                    curSum = nums[idx];
+                }
+            }
+            else // num >= 0
+            {
+                if (curSum < 0)
+                {
+                    sum = Math.Max(sum, nums[idx]);
+                    curSum = nums[idx];
+                }
+                else // curSum >= 0 
+                {
+                    curSum += nums[idx];
+                    // do not update here to aovid multi update
+                }
+            }
+        }
+
+        return Math.Max(curSum, sum);
+    }
+}
