a2ff5b24efea38225fce0929215a5d88ff1ac599
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-16 17:10:41 +0800

    101. Symmetric Tree

---
+++ Leetcode/Leetcode/Tree/SymmetricTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ *
+ * @jingjiejiang Apr 16, 2019
+ */
+public class Class1
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        if (root == null) return true;
+
+        return checkSym(root.left, root.right);
+    }
+
+    private bool checkSym(TreeNode root1, TreeNode root2)
+    {
+        if (root1 == null && root2 == null)
+        {
+            return true;
+        }
+
+        if (root1 == null || root2 == null)
+        {
+            return false;
+        }
+
+        return checkSym(root1.left, root2.right) & checkSym(root1.right, root2.left);
+    }
+}
