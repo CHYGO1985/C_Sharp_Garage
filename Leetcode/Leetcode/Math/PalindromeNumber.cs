2415fa534eab5f1db5058778e4cd1453a7fcd234
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-18 11:29:21 +0800

    9. Palindrome Number

---
+++ Leetcode/Leetcode/Math/PalindromeNumber.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+
+/*
+ * @jingjiejiang Apr 18, 2019
+ */ 
+public class Class1
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+        if (x < 10) return true; // 0 return true
+        if (x % 10 == 0) return false;
+
+        int oldNum = x;
+        int newNum = x % 10;
+        x /= 10;
+
+        while (x > 0)
+        {
+            newNum = newNum * 10 + x % 10;
+            x /= 10;
+        }
+
+        return newNum == oldNum ? true : false;
+    }
+}
