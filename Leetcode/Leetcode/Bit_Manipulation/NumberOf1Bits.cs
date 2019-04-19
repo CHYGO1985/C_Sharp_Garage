7de3b5f4789eb3429e508b99fd433b6a8f6fa7d9
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-19 14:12:24 +0800

    191. Number of 1 Bits

---
+++ Leetcode/Leetcode/Bit_Manipulation/NumberOf1Bits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Apr 19, 2019
+ */
+public class Class1
+{
+    public int HammingWeight(uint n)
+    {
+        int ans = 0;
+        while (n != 0)
+        {
+            ans += (int)n & 1;
+            n >>= 1;
+        }
+        return ans;
+    }
+}
