b0670170a5f32b3a281a02cde0c6ccc41265ba5c
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-19 16:49:44 +0800

    83. Remove Duplicates from Sorted List

---
+++ Leetcode/Leetcode/List/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Apr 19, 2019
+ */ 
+public class Class1
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        ListNode curHead = head;
+
+        while (curHead != null && curHead.next != null) {
+
+            ListNode shiftNode = curHead.next;
+
+            while (shiftNode != null && shiftNode.val == curHead.val)
+            {
+                shiftNode = shiftNode.next;
+            }
+
+            curHead.next = shiftNode;
+            curHead = curHead.next;
+        }
+
+        return head;
+    }
+}
