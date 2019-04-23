299166a4057305cb74b6eda0bd3cc7fa839a040b
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-23 18:19:24 +0800

    init commit

---
+++ Leetcode/Leetcode/Array/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Apr 23, 2019
+ */ 
+public class Class1
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums == null || nums.Length == 0) return 0;
+
+        int head = 1, rear = nums.Length - 2;
+        int headNum = nums[head - 1], rearNum = nums[rear + 1];
+
+        while (headNum < rearNum && headNum != rearNum)
+        {
+            while (headNum < rearNum && nums[head] != headNum)
+            {
+                headNum = nums[head];
+                head ++;
+            }
+
+            while (headNum < rearNum && nums[rear] != rearNum)
+            {
+                rearNum = nums[rear];
+                rear --;
+            } 
+
+            if (nums[head] != nums[rear])
+
+        }
+
+        return 
+    }
+
+    private void swap(int[] nums, int head, int rear)
+    {
+        int tmp = nums[head];
+        nums[head] = nums[rear];
+        nums[rear] = tmp;
+    }
+}
---
+++ Leetcode/Leetcode/Tree/BalancedBinaryTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Apr 23, 2019
+ */ 
+public class Class1
+{
+    public bool IsBalanced(TreeNode root)
+    {
+        bool[] res = new bool[1];
+        res[0] = true;
+
+        check(root, res);
+
+        return res[0];
+    }
+
+    private int check(TreeNode root, bool[] res)
+    {
+        if (root == null) return 0;
+
+        int left = check(root.left, res);
+        int right = check(root.right, res);
+
+        if (Math.Abs(left - right) > 1) res[0] = false;
+
+        return Math.Max(left, right) + 1;
+    }
+}
