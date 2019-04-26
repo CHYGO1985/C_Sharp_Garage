649fb86acbb7fe30cb37a3bd59edbb854985c1a4
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-26 10:26:44 +0800

    172. Factorial Trailing Zeroes

---
+++ Leetcode/Leetcode/Math/FactorialTrailingZeroes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Apr 26, 2019
+ * ref: https://leetcode.com/problems/factorial-trailing-zeroes/discuss/52371/My-one-line-solutions-in-3-languages
+ */
+public class Class1
+{
+    public int TrailingZeroes(int n)
+    {
+        return n == 0 ? 0 : n / 5 + TrailingZeroes(n / 5);
+    }
+}
