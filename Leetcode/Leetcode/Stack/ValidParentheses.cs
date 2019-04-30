a306d4c6f8bd6e9b1a666924187f623977403b9b
JJ Jiang <jingjiejiang85@gmail.com>
2019-04-30 16:26:16 +0800

    20. Valid Parentheses

---
+++ Leetcode/Leetcode/Stack/ValidParentheses.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+/*
+ * @jingjiejiang Apr 30, 2019
+ * 
+ */ 
+public class Class1
+{
+    public bool IsValid(string s)
+    {
+        if (s == null) return true;
+
+        Stack charsStack = new Stack();
+
+        foreach (char character in s)
+        {
+            if (character == '(' || character == '[' || character == '{')
+            {
+                charsStack.Push(character);
+            }
+            else
+            {
+                if (charsStack.Count == 0) return false;
+                StackFiller(charsStack, character);
+            }
+        }
+
+        return charsStack.Count == 0 ? true : false;
+    }
+
+    private void StackFiller(Stack stack, char character)
+    {
+
+        switch (character)
+        {
+            case ')' :
+                if ((char)stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(character);
+                }
+                break;
+            case '}':
+                if ((char)stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(character);
+                }
+                break;
+            case ']':
+                if ((char)stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(character);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
