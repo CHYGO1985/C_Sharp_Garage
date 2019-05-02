dcf0451aa53ca1b2da586ac04ca942aaac4af08a
JJ Jiang <jingjiejiang85@gmail.com>
2019-05-02 15:06:10 +0800

    88. Merge Sorted Array

---
+++ Leetcode/Leetcode/Array/MergeSortedArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Mar 2, 2019
+ */ 
+public class Class1
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int finalTail = nums1.Length - 1, nums1Tail = m - 1, nums2Tail = n - 1;
+        
+        while (nums1Tail >= 0 && nums2Tail >= 0)
+        {
+            nums1[finalTail --] = (nums1[nums1Tail] >= nums2[nums2Tail]) ? nums1[nums1Tail --] : nums2[nums2Tail --];
+        }
+
+        while (nums1Tail >= 0)
+        {
+            nums1[finalTail --] = nums1[nums1Tail--];
+        }
+
+        while (nums2Tail >= 0)
+        {
+            nums1[finalTail--] = nums2[nums2Tail--];
+        }
+    }
+}
