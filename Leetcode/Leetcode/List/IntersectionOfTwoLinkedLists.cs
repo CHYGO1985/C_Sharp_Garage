f4f004ccc14bc9effa4dd57faa58f635ea8e56c8
JJ Jiang <jingjiejiang85@gmail.com>
2019-05-03 13:37:46 +0800

    160. Intersection of Two Linked Lists

--- Leetcode/Leetcode/List/IntersectionOfTwoLinkedLists.cs
+++ Leetcode/Leetcode/List/IntersectionOfTwoLinkedLists.cs
@@ -2,34 +2,52 @@
 
 /*
  * 
- * @jingjiejiang Mar 2, 2019
+ * @jingjiejiang Mar 3, 2019
  */ 
 public class Class1
 {
     public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
     {
         // reverse A and B, then get the first node that is different
-
         if (headA == null || headB == null) return null;
-       
-        ListNode newHeadA = reverse(headA);
-        ListNode newHeadB = reverse(headB);
 
-        if (newHeadA != newHeadB) return null;
+        int lenA = 0, lenB = 0;
 
-        while (newHeadA != null && newHeadB != null)
+        lenA = GetLength(headA);
+        lenB = GetLength(headB);
+
+        ListNode headLong = lenA >= lenB ? headA : headB;
+        ListNode headShort = lenA >= lenB ? headB : headA;
+        int longLen = lenA >= lenB ? lenA : lenB;
+        int shortLen = lenA >= lenB ? lenB : lenA;
+
+        while (longLen > shortLen)
         {
+            if (headLong == headShort) return headShort;
+            headLong = headLong.next;
+            longLen--;
         }
+
+        while (shortLen != null && headLong != null)
+        {
+            if (headLong == headShort) return headShort;
+            headLong = headLong.next;
+            headShort = headShort.next;
+        }
+
+        return null;
     }
 
-    private ListNode reverse(ListNode head)
+    private int GetLength(ListNode head)
     {
-        if (head == null || head.next == null) return head;
+        int len = 0;
 
-        ListNode newHead = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
+        while (head != null)
+        {
+            len++;
+            head = head.next;
+        }
 
-        return newHead;
+        return len;
     }
 }
