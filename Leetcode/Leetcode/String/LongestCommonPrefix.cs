5701d6ad650cc3896c3f2e8c9f3786116909d5a7
JJ Jiang <jingjiejiang85@gmail.com>
2019-05-03 14:51:47 +0800

    14. Longest Common Prefix Optimised method

---
+++ Leetcode/Leetcode/String/LongestCommonPrefix.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+/*
+ * 
+ * @jingjiejiang Mar 3, 2019
+ */
+public class Class1
+{
+    //public string LongestCommonPrefix(string[] strs)
+    //{
+    //    // find the shortest in array, then compare via using the shortest
+    //    string res = "";
+    //    if (strs == null || strs.Length == 0) return res;
+
+    //    string shortStr = strs[0];
+        
+    //    foreach(string str in strs)
+    //    {
+    //        if (str.Length < shortStr.Length)
+    //        {
+    //            shortStr = str;
+    //        }
+    //    }
+
+    //    int length = 1;
+
+    //    while (length <= shortStr.Length)
+    //    {
+    //        string prefix = shortStr.Substring(0, length);
+    //        foreach (string str in strs)
+    //        {
+    //            if (str.IndexOf(prefix) != 0) return res;
+    //        }
+
+    //        res = prefix;
+    //        length++;
+    //    }
+
+    //    return res;
+    //}
+
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string res = "";
+        if (strs == null || strs.Length == 0) return res;
+
+        string standard = strs[0];
+        StringBuilder builder = new StringBuilder();
+
+        for (int idx = 0; idx < standard.Length; idx++)
+        {
+            char curChar = standard[idx];
+            for (int strIdx = 1; strIdx < strs.Length; strIdx++)
+            {
+                if (idx >= strs[strIdx].Length || strs[strIdx][idx] != curChar) return builder.ToString();
+            }
+            builder.Append(curChar);
+        }
+
+        return builder.ToString();
+    }
+}
